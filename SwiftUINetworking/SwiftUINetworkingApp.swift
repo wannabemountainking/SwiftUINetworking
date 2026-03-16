dc17016017d88cf9c66180b9426acf7926c44bae
wannabemountainking <kdy0096@gmail.com>
2026-03-16 16:47:36 +0900

    Initial Commit

---
+++ SwiftUINetworking/SwiftUINetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINetworkingApp.swift
+//  SwiftUINetworking
+//
+//  Created by YoonieMac on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
