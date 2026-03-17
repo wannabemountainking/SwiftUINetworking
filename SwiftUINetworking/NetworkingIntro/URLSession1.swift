28c3131f78d45ab275dfc09e10d1c10f27615bba
wannabemountainking <kdy0096@gmail.com>
2026-03-17 12:17:02 +0900

    URLSession & URLSessionDataTask

--- SwiftUINetworking/NetworkingIntro/URLSession1.swift
+++ SwiftUINetworking/NetworkingIntro/URLSession1.swift
@@ -26,33 +26,61 @@ struct RandomUser: Codable {
     let age: Int
 }
 
+// MARK: - Error Type
+enum RandomError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
+
 // MARK: - UI
 struct URLSession1: View {
     
-    @State private var user: RandomUser = RandomUser(username: "user name", image: "", email: "google@gmail.com", age: 0)
+    @State private var user: RandomUser?
+    @State private var userImage: UIImage = UIImage(systemName: "person")!
     
     var body: some View {
         VStack(spacing: 20) {
-            // image
-            Circle()
-                .foregroundStyle(.secondary)
-                .frame(width: 200, height: 200)
+            
+            //TODO: AsyncImage 사용하기
+            AsyncImage(url: URL(string: user?.image ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            } placeholder: {
+                // image
+                Circle()
+                    .foregroundStyle(.secondary)
+            }
+            .frame(width: 200, height: 200)
+            
             // username age
             HStack(spacing: 20) {
-                Text(user.username)
+                Text(user?.username ?? "User name")
                     .font(.title.bold())
-                Text("\(user.age)")
+                Text("\(user?.age ?? 0)")
                     .font(.title.bold())
             }
             // email
-            Text(user.email)
+            Text(user?.email ?? "google@gmail.com")
                 .font(.title2)
                 .padding()
             
             // refresh button
             Button(action: {
                 Task {
-                    user = await getUser()
+                    do {
+                        user = try await getUser()
+                    } catch RandomError.invalidURL {
+                        print("invalid URL")
+                    } catch RandomError.invalidResponse {
+                        print("invalid Response")
+                    } catch RandomError.invalidData {
+                        print("invalid Data")
+                    } catch {
+                        print("I don't know: \(error)")
+                    }
                 }
             }, label: {
                 Text("Get Random User")
@@ -68,20 +96,25 @@ struct URLSession1: View {
     }
     
     // MARK: - Function
-    private func getUser() async -> RandomUser {
-        let urlString = "https://dummyjson.com/users/\(Int.random(in: 1...100))"
-        let url = URL(string: urlString)!
-        let session = URLSession.shared
+    private func getUser() async throws -> RandomUser {
+        let randomNum = Int.random(in: 1...100)
+        let endPoint = "https://dummyjson.com/users/\(randomNum)"
+        // url이 옵셔널 값이기 때문에 error 처리 안하고 강제로 ! 붙여서 하기
+        //        let url = URL(string: endPoint)!
+        
+        // 정공법
+        guard let url = URL(string: endPoint) else { throw RandomError.invalidURL }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw RandomError.invalidResponse
+        }
         do {
-            let (data, _) = try await session.data(from: url)
             let decoder = JSONDecoder()
-            let randomUser = try decoder.decode(RandomUser.self, from: data)
-            return randomUser
+            return try decoder.decode(RandomUser.self, from: data)
         } catch {
-            fatalError("Error on Getting Data: \(error)")
+            throw RandomError.invalidData
         }
     }
-    
 }
 
 #Preview {
