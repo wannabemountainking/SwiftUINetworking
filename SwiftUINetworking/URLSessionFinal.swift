f5c38af2025578b372673034468eb24c38f1263a
김도윤 <kdy0096@gmail.com>
2026-03-21 01:34:15 +0900

    finished

--- SwiftUINetworking/URLSessionFinal.swift
+++ SwiftUINetworking/URLSessionFinal.swift
@@ -21,32 +21,64 @@ final class URLSessionFinalViewModel {
     var cancellable: Set<AnyCancellable> = Set<AnyCancellable>()
     
     // Error 처리 위한 함수
-    func handleResponse(data: Data?, response: HTTPURLResponse?) -> UIImage? {
+    func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
         guard let data,
               let image = UIImage(data: data),
               let response = response as? HTTPURLResponse,
-              response.statusCode >= 200 && response.statusCode < 300 else
-        {return nil}
+              response.statusCode >= 200 && response.statusCode < 300 else {return nil}
         return image
     }
     
     // MARK: - 이미지 가져오기
     // MARK: - @escaping
     func fetchImage() {
-        
-    }
-    
-    func downloadWithEscaping(completion: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
-        guard let url = URL(string: endPoint) else {return}
-        let task = URLSession.shared.dataTask(with: url) {[weak self] data, res, err in
-            guard let self else {return}
-            let image = handleResponse(data: data, response: res as! HTTPURLResponse)
+        downloadWithEscaping {[weak self] image, error in
+            if let image = image {
+                guard let self else {return}
+                self.imageFromEscaping = image
+            }
         }
     }
+    func downloadWithEscaping(completion: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
+        guard let url = URL(string: self.endPoint) else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, res, err in
+            guard let self else {return}
+            let image = self.handleResponse(data: data, response: res)
+            completion(image, err)
+        }
+        task.resume()
+    }
+    
     // MARK: - Combine
+    func downloadWithCombine() {
+        // 단순히 이미지만 받는 것이라 .subscribe, .receive생략 => 근데 나는 연습용으로 쓸것임
+        guard let url = URL(string: self.endPoint) else {return}
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .map(handleResponse)
+            .mapError( { $0 } )
+            .sink { completion in
+            } receiveValue: { [weak self] image in
+                guard let self else {return}
+                self.imageFromCombine = image
+            }
+            .store(in: &cancellable)
+    }
     
     // MARK: - Async
-    
+    func downloadWithAsync() async throws {
+        guard let url = URL(string: self.endPoint) else {return}
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            let image = handleResponse(data: data, response: response)
+            self.imageFromAsync = image
+        } catch {
+            throw error
+        }
+        
+    }
 }
 
 struct URLSessionFinal: View {
@@ -82,7 +114,7 @@ struct URLSessionFinal: View {
 
                     Divider()
                     
-                    Text("1. Combine")
+                    Text("2. Combine")
                         .font(.title)
                     
                     if let image = vm.imageFromCombine {
@@ -97,7 +129,7 @@ struct URLSessionFinal: View {
                     }
                     
                     Button(action: {
-                        
+                        vm.downloadWithCombine()
                     }, label: {
                         Text("Download Image with Combine")
                             .frame(maxWidth: .infinity)
@@ -108,7 +140,7 @@ struct URLSessionFinal: View {
 
                     Divider()
                     
-                    Text("1. Async")
+                    Text("3. Async")
                         .font(.title)
                     
                     if let image = vm.imageFromAsync {
@@ -123,7 +155,13 @@ struct URLSessionFinal: View {
                     }
                     
                     Button(action: {
-                        
+                        Task {
+                            do {
+                                try await vm.downloadWithAsync()
+                            } catch {
+                                print(error)
+                            }
+                        }
                     }, label: {
                         Text("Download Image with Async")
                             .frame(maxWidth: .infinity)
