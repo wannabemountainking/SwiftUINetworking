588ce233e667bfabefb0694795a26c41aa557e53
김도윤 <kdy0096@gmail.com>
2026-03-20 15:56:52 +0900

    initial commit

---
+++ SwiftUINetworking/URLSessionFinal.swift
@@ -0,0 +1,136 @@
+//
+//  URLSessionFinal.swift
+//  SwiftUINetworking
+//
+//  Created by yoonie on 3/20/26.
+//
+
+// endPoint: https://picsum.photos/200
+
+import SwiftUI
+import Combine
+
+
+@Observable
+final class URLSessionFinalViewModel {
+    var imageFromEscaping: UIImage? = nil
+    var imageFromCombine: UIImage? = nil
+    var imageFromAsync: UIImage? = nil
+    
+    let endPoint: String = "https://picsum.photos/200"
+    var cancellable: Set<AnyCancellable> = Set<AnyCancellable>()
+    
+    // Error 처리 위한 함수
+    func handleResponse(data: Data?, response: URLResponse?) throws -> UIImage? {
+        guard let data,
+              let image = UIImage(data: data),
+              let response = response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {return nil}
+        return image
+    }
+    
+    // MARK: - 이미지 가져오기
+    // MARK: - @escaping
+    
+    // MARK: - Combine
+    
+    // MARK: - Async
+    
+}
+
+struct URLSessionFinal: View {
+    @State private var vm: URLSessionFinalViewModel = .init()
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    Text("1. @escaping")
+                        .font(.title)
+                    
+                    if let image = vm.imageFromEscaping {
+                        Image(uiImage: image)
+                            .resizable()
+                            .frame(width: 200, height: 200)
+                            .clipShape(Circle())
+                    } else {
+                        Circle()
+                            .frame(width: 200, height: 200)
+                            .clipShape(Circle())
+                    }
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Download Image with @escaping")
+                            .frame(maxWidth: .infinity)
+                            .font(.title2)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .padding(.horizontal)
+
+                    Divider()
+                    
+                    Text("1. Combine")
+                        .font(.title)
+                    
+                    if let image = vm.imageFromCombine {
+                        Image(uiImage: image)
+                            .resizable()
+                            .frame(width: 200, height: 200)
+                            .clipShape(Circle())
+                    } else {
+                        Circle()
+                            .frame(width: 200, height: 200)
+                            .clipShape(Circle())
+                    }
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Download Image with Combine")
+                            .frame(maxWidth: .infinity)
+                            .font(.title2)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .padding(.horizontal)
+
+                    Divider()
+                    
+                    Text("1. Async")
+                        .font(.title)
+                    
+                    if let image = vm.imageFromAsync {
+                        Image(uiImage: image)
+                            .resizable()
+                            .frame(width: 200, height: 200)
+                            .clipShape(Circle())
+                    } else {
+                        Circle()
+                            .frame(width: 200, height: 200)
+                            .clipShape(Circle())
+                    }
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Download Image with Async")
+                            .frame(maxWidth: .infinity)
+                            .font(.title2)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .padding(.horizontal)
+
+                    Divider()
+                    
+                } //: VSTACK
+            } //:SCROLL
+            .navigationTitle("@escaping vs Combine vs Async")
+            .navigationBarTitleDisplayMode(.inline)
+        }//: NAVIGATIONSTACK
+    }//: body
+}
+
+#Preview {
+    URLSessionFinal()
+}
