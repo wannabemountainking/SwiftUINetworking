375e6e4779780bf5905a12bed12bf12438ab0648
김도윤 <kdy0096@gmail.com>
2026-03-27 15:47:10 +0900

    Combine Timer

--- SwiftUINetworking/Combine/Timer1.swift
+++ SwiftUINetworking/Combine/Timer1.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct Timer1: View {
     
@@ -15,9 +16,17 @@ struct Timer1: View {
      every: 시간 Interval로 1.0은 1초를 가리킴
      on: Thread 설정 (UI업데이트이니까 .main)
      in: loop를 어떤 식으로 하는 지에 대한 설정
-     .autoconnect() : Publisher Timer가 UI 
+     .autoconnect() : Publisher Timer가 UI가 업데이트 될 때마다 시작할 수 있게 함 (매번 초기화해서 업데이트)
      */
-    let Timer1 = Timer.publish(every: 1.0, on: .main, in: <#T##RunLoop.Mode#>)
+    let Timer1 = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    
+    // 1. 현재 시간 변수
+    @State private var currentDate: Date = Date()
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        return formatter
+    }
     
     // MARK: - UI
     var body: some View {
