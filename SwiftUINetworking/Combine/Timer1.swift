f611f9ee3a9fd4d74a756983e93ef54e49de5a62
wannabemountainking <kdy0096@gmail.com>
2026-03-27 17:51:00 +0900

    timer countdown

--- SwiftUINetworking/Combine/Timer1.swift
+++ SwiftUINetworking/Combine/Timer1.swift
@@ -18,7 +18,7 @@ struct Timer1: View {
      in: loop를 어떤 식으로 하는 지에 대한 설정
      .autoconnect() : Publisher Timer가 UI가 업데이트 될 때마다 시작할 수 있게 함 (매번 초기화해서 업데이트)
      */
-    let Timer1 = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    let timer1 = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
     
     // 1. 현재 시간 변수
     @State private var currentDate: Date = Date()
@@ -28,10 +28,43 @@ struct Timer1: View {
         return formatter
     }
     
+    // 2. 카운트다운 타이머
+    @State private var count: Int = 10
+    @State private var finishedText: String? = nil
+    
     // MARK: - UI
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        NavigationStack {
+            VStack(spacing: 20) {
+                // 1. 현재시간 타이머
+                Text("1. 현재 시간 Timer")
+                Text(dateFormatter.string(from: currentDate))
+                    .foregroundStyle(.accent)
+                
+                // 2. 카운트 다운: 10, 9, ....0
+                Text("2. 카운트 다운 Timer")
+                Text(finishedText ?? "\(count)")
+                    .foregroundStyle(.accent)
+            } //:VSTACK
+            .font(.largeTitle.bold())
+            .navigationTitle("Timer")
+            
+            // .onReceive: Subscriber 역할이며 Publisher(여기서는 timer1)의 value 를 가져와서 UI에 실행 시켜줌
+            // 1. 현재시간 .onReceive
+            .onReceive(timer1) { output in
+                self.currentDate = output
+            }
+            
+            // 2. 카운트 다운 .onReceive
+            .onReceive(timer1) { _ in
+                if self.count <= 1 {
+                    finishedText = "시작!"
+                } else {
+                    self.count -= 1
+                }
+            }
+        } //:NAVIGATION
+    }//:body
     
     // MARK: - Function
     
