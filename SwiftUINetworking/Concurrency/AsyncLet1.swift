1109c182cf76ee3a7402b0c3eb8866f767f05e40
김도윤 <kdy0096@gmail.com>
2026-04-30 01:19:12 +0900

    initializing Asynclet

---
+++ SwiftUINetworking/Concurrency/AsyncLet1.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncLet1.swift
+//  SwiftUINetworking
+//
+//  Created by yoonie on 4/30/26.
+//
+
+import SwiftUI
+
+struct AsyncLet1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncLet1()
+}
